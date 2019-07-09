7695f66c0a0a252fdc640ff90745ee707cb745fc
somya <somya.singhal0797@gmail.com>
2019-07-09 14:02:13 +0530

    mongoose connection mlab

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
 
 const port = 3000
 
 const app = express();
 
+app.use(bodyParser.json())
+
+const server = 'ds341837.mlab.com:41837'
+const database = 'learning'
+const user = 'nitinkumar24'
+const password = 'omg123omg123'
+
+
 mongoose.connect("mongodb://test:test123@ds341837.mlab.com:41837/learning");
 
 
-app.get('/', (req,res) => {
-  res.send("hello with express");
-
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome  application"});
 });
+
 
 app.listen(port, () => {
    console.log(`server started on port no ${port}`)
